2c8f898b3bad7c93d7a1602ad5a29af23c96270d
Mehtaab Khan <meetmak9@gmail.com>
2021-08-15 22:35:49 +0100

    Just saving the file because I've been trying to firgure out this dumb error

--- Pycrosoft Plebe/app.py
+++ Pycrosoft Plebe/app.py
@@ -25,10 +25,6 @@ while True:
     ask_password = input('')
     if ask_password == password:
         break
-        print("You're logged in")
-        print('Hello stupid1234')
-        today = datetime.date.today()
-        time = '{today.day}/{today.month}/{today.year}'.format(today=today)
         print("The date today is" + time)     
     elif ask_password != password:
         print('You have to type a password')
@@ -36,3 +32,8 @@ while True:
         if incorrectCount == 12:
             print("You're busted, hacker! You can't log in now!")
             break
+
+print("You're logged in")
+print('Hello stupid1234')        
+today = datetime.date.today()
+time = '{today.day}/{today.month}/{today.year}'.format(today=today)
