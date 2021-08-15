ca4acc8f0ced8b2b2aae455e9fdf96fb5bcf0f51
Mehtaab Khan <meetmak9@gmail.com>
2021-08-15 19:59:34 +0100

    Changed a few parts of the code

--- Pycrosoft Plebe/app.py
+++ Pycrosoft Plebe/app.py
@@ -23,17 +23,15 @@ for i in range(56):
 while True:
     print('What is the password?')
     ask_password = input('')
-    if ask_password != password:
-        print('You have to type a password')
-        incorrectCount += 1
-        if incorrectCount == 12:
-            print("You're busted, hacker! You can't log in now!")
-            break
-    elif ask_password == password:
-        break
+    if ask_password == password:
         print("You're logged in")
         print('Hello stupid1234')
         today = datetime.date.today()
         time = '{today.day}/{today.month}/{today.year}'.format(today=today)
-        print(time)
-        
+        print(time)     
+    elif ask_password != password:
+        break
+        print('You have to type a password')
+        incorrectCount += 1
+        if incorrectCount == 12:
+            print("You're busted, hacker! You can't log in now!")
