bc31c0a484d6ebb93c315ba010c4b0b5a1483e0c
Mehtaab Khan <meetmak9@gmail.com>
2021-09-13 21:43:28 +0100

    I am gonna reimagine the Pycrosoft Pleb program into something more better than the last one

--- Pycrosoft Pleb/app.py
+++ Pycrosoft Pleb/app.py
@@ -1,44 +1,6 @@
-from time import time
-from typing import Text
-import webbrowser, datetime
+import tkinter as tk
 
-password, incorrectCount = '', 0
 
-def settingUp():
-    global password
-    while True:
-        print('Type a password: ')
-        password = input('')
-        if password == '':
-            print('You need to type something')
-        else:
-            print('Done!')
-            break
-    print('This is your password: ' + password)
-    print('Restarting...')
-settingUp()
-
-for i in range(56):
-    print('Restarting...')
-while True:
-    print('What is the password?')
-    ask_password = input('')
-    if ask_password == password:
-        break    
-    elif ask_password != password:
-        print('You have to type a password')
-        incorrectCount += 1
-        if incorrectCount == 12:
-            print("You're busted, hacker! You can't log in now! We're locking the account! HAH!")
-            break
-
-print("You're logged in")
-print('Hello stupid1234')        
-today = datetime.date.today()
-time = '{today.day}/{today.month}/{today.year}'.format(today=today)
-print("The date today is" + time)
-while True:
-    a_url = input('Type a URL: ')
-    print('Redirecting you to ' + a_url)
-    print('Redirecting successful!')
-    webbrowser.open_new(a_url)
+def register():
+    db = open('database.txt', 'r')
+    pass
